fd18eecf1a5264d9952a946df6c7d2e4a6883634
jothax <jonathanbiel456@gmail.com>
2025-03-28 10:45:05 -0300

    Refatoring

--- C++/Lamps.cpp
+++ C++/Lamps.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
+
 //Fonts:https://neps.academy/br/exercise/52
-void change(bool& value)
-{
-    if(value == false){
-        value = true;
-    }else{
-        value = false;
-    }
-}
 
 int main()
 {
@@ -19,20 +12,20 @@ int main()
     {
         std::cout<<"Quarto Tailandia"<<'\n'
         <<"Digite 1 para acender a lampada A"<<'\n'
-        <<"Digite 2 para acender a lampada B e desligar a A"<<'\n'
+        <<"Digite 2 para acender a lampada B"<<'\n'
         <<"Digite 0 para sair"<<'\n';
 
         std::cin>>choice;
         switch (choice)
         {
             case 1:
-                change(A);
+                A = !A;
                 std::cout<<"Lampada A: "<<A<<'\n'
                 <<"Lampada B: "<<B<<'\n';
                 break;
             case 2:
-                change(A);
-                change(B);
+                A = !A;
+                B = !B;
                 std::cout<<"Lampada A: "<<A<<'\n'
                 <<"Lampada B: "<<B<<'\n';
                 break;
