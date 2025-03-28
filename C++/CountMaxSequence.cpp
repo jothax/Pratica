e2584cb23779439defa20e6d8082c3f837104642
jothax <jonathanbiel456@gmail.com>
2025-03-28 10:14:49 -0300

    CountMaxSequence.cpp

---
+++ C++/CountMaxSequence.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+/*
+    https://neps.academy/br/exercise/110
+*/
+
+int main()
+{
+    int countA {0};
+    int countB {0};
+    int lenght {0};
+    int digitado{0};
+
+    int guarda;
+    int avanca;
+
+    std::vector<int> list(0);
+
+    //tamanho da lista
+    std::cout<<"Quantos Numeros vai digitar?: ";
+    std::cin>>lenght;
+    list.resize(lenght);
+
+    //encher a lista
+    for(int i = 0; i < lenght;i++)
+    {
+        std::cout<<"Digite o "<<i<<"° numero: ";
+        std::cin>>digitado;
+        list[i] = digitado;
+    }
+
+    guarda = list[0];
+    //pecorrer a lista e marcar os iguais
+    for(int i=0; i<list.size();i++)
+    {
+        avanca = list[i];
+        
+        if(guarda != avanca)
+        {
+            guarda = list[i];
+            i--;
+            countB = countA;
+            if(countB>=countA)countA=0;
+        }
+        else
+        {
+            countA++;
+            if(countA>countB)countB = countA;
+        }
+        
+    }
+
+    std::cout<<'\n'<<countB<<'\n';
+
+    return 0;
+}
